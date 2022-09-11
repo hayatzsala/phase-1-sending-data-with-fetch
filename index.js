e860be851632bbb8858d7bcdd28a5b9725047395
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-09-12 01:57:05 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, email){
+    return fetch('http://localhost:3000/users',
+    {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "name": "Steve",
+            "email": "steve@steve.com"
+        })
+
+    })
+    .then(response => response.json())
+    .then(data =>{
+        const id = data.id;
+        document.body.innerHTML = id;
+    })
+    .catch(error => {
+        document.body.innerHTML = error;
+    });
+}
